a09a8adaf8941887eb0c7a406bd1b0264ae42180
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-01-22 12:29:32 +0300

    add README

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -35,6 +35,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns the page with the correct offset and limit"""
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
 
