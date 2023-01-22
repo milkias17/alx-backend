72912ff0b8e4d3b9ffa2de53be0495e3b43bd770
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-01-22 18:51:43 +0300

    hypermedia_pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -2,7 +2,7 @@
 """Pagination"""
 import csv
 import math
-from typing import Dict, List
+from typing import List, Dict
 
 
 def index_range(page: int, page_size: int):
@@ -47,15 +47,26 @@ class Server:
         return current_dataset[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+        returns a dictionary containing the following key-value pairs:
+
+        page_size: the length of the returned dataset page
+        page: the current page number
+        data: the dataset page (equivalent to return from previous task)
+        next_page: number of the next page, None if no next page
+        prev_page: number of the previous page, None if no previous page
+        total_pages: the total number of pages in the dataset as an integer
+        """
         page_data = self.get_page(page, page_size)
-        total_page_num = math.ceil(len(self.dataset()) / page_size)
-        next_page = page + 1 if page < total_page_num else None
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
 
         return dict(
             page_size=len(page_data),
-            page=page, data=page_data,
+            page=page,
+            data=page_data,
             next_page=next_page,
             prev_page=prev_page,
-            total_pages=total_page_num
+            total_pages=total_pages
         )
