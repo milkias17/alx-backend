c92f4abb19eee805e97f0741fab19f112261620d
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-01-22 19:01:01 +0300

    add docstring

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,18 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        return a dictionary with the following key-value pairs:
+            index: the current start index of the return page.
+                That is the index of the first item in the current page.
+                For example if requesting page 3 with page_size 20, and no data
+                was removed from the dataset, the current index should be 60.
+            next_index: the next index to query with.
+                That should be the index of the first item
+                after the last item on the current page.
+            page_size: the current page size
+            data: the actual page of the dataset
+    """
         indexed_data = self.indexed_dataset()
         assert isinstance(index, int) and index in range(len(indexed_data))
 
